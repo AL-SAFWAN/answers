b82b2540b57e2e490b62586e0bc5691c2dddb882
AL-SAFWAN <51613782+AL-SAFWAN@users.noreply.github.com>
2019-07-18 11:21:06 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int y = 2;
+	int x = 3;
+	// Sum Of Two Numbers
+        int sum = x+y ;
+        System.out.println(sum);
+    }
+}
