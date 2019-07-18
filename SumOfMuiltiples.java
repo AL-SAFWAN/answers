a3a0db2aff467b590d246d325634b7b1c20e4ba2
AL-SAFWAN <51613782+AL-SAFWAN@users.noreply.github.com>
2019-07-18 15:03:20 +0100

    Add files via upload

---
+++ SumOfMuiltiples.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class SumOfMuiltiples {
+
+    public static void theSum(int x, int y, int z)
+
+    {
+
+
+        // Sum Of Two Numbers
+        int sum = x * y * z;
+
+
+        System.out.println(sum );
+
+    }
+
+    public static void Sum(int x, int y){
+        int sum = x* y;
+        System.out.println(sum);
+    }
+}
---
+++ sumOfTwoNumbers.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class sumOfTwoNumbers {
+
+    public static void theSum(int x, int y, int z)
+
+    {
+
+
+        // Sum Of Two Numbers
+        int sum = x + y + z;
+
+
+        System.out.println(sum );
+
+    }
+
+
+}
